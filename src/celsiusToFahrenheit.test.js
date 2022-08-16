6c78f18a2759ea1c3f3a3f1f7a4e0fb5a697622b
Andrés Rojas <andresroberto10@gmail.com>
2022-08-16 15:53:37 +0200

    Fix test description

--- src/celsiusToFahrenheit.test.js
+++ src/celsiusToFahrenheit.test.js
@@ -1,5 +1,5 @@
 const celsiusToFahrenheit = require('./celsiusToFahrenheit');
 
-test('converts from fahrenheit to celsius', () => {
+test('converts from celsius to fahrenheit', () => {
   expect(celsiusToFahrenheit(25)).toBe(77);
 });
